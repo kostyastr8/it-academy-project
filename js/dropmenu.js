ac101e29696985cd85a7f80b1626da5bc2eaa0c3
Konstantyn <kostyastr8@gmail.com>
2016-11-21 23:27:13 +0200

    add different adaptive @media

---
+++ js/dropmenu.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $('.header-nav li').hover(
+      function() {
+        $('ul', this).slideDown(110);
+      },
+      function() {
+        $('ul', this).slideUp(110);
+      }
+    );
+  });
+
